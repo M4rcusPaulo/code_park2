6f6a780c11dc88cc84d74d1960cdd25439f07d8e
MarcusPaulo <marcuscaetanocloud@outlook.com>
2025-03-24 22:16:57 -0300

    Primeiro commit desse projeto

---
+++ script.js
@@ -0,0 +1,23 @@
+const titulo = document.getElementById("h1");
+const listaNaoOrdenada = document.querySelector("ul");
+const link = document.querySelector("a");
+const listaOrdenada = document.getElementById("lista-ordenada");
+
+titulo.innerText = "Seja bem-vindo ao meu projeto!";
+link.innerText = "Acesse o site da Proz Educação";
+
+listaNaoOrdenada.innerHTML =
+`<li>Item 1</li>
+<li>Item 2</li>
+<li>Item 3</li>`;
+
+listaOrdenada.innerHTML = `
+   <li><a 
+   href="https://www.google.com"
+target="_blank">Google</a></li>
+   <li><a
+href="https://wikipedia.org"
+target="_blank">Wikipedia</a></li>
+    <li><a
+href="https://www.github.com"
+target="_blank">GitHub</a></li>`;
